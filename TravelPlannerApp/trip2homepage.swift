156c330bab9f0301b00523b75b37ae092ab72170
Joyce Yang <pinkeesmur@gmail.com>
2025-07-30 13:39:02 -0500

    jj

---
+++ TravelPlannerApp/trip2homepage.swift
@@ -0,0 +1,18 @@
+//
+//  trip2homepage.swift
+//  TravelPlannerApp
+//
+//  Created by Joyce Yang on 7/30/25.
+//
+
+import SwiftUI
+
+struct trip2homepage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    trip2homepage()
+}
