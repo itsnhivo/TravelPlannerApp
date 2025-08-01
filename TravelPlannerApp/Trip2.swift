7c4bbef0ec76d26b3dba7095aa786c6d6d9ab8cc
Nhi <nhivo0123@gmail.com>
2025-08-01 12:53:27 -0500

    added trip2

---
+++ TravelPlannerApp/Trip2.swift
@@ -0,0 +1,18 @@
+//
+//  Trip2.swift
+//  TravelPlannerApp
+//
+//  Created by Scholar on 8/1/25.
+//
+
+import SwiftUI
+
+struct Trip2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Trip2()
+}
